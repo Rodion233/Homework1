68c2661cf9e9e83d03635167a332cf1406cc8a4d
Rodion23 <rodion23@mail.ru>
2023-01-25 11:14:02 +0700

    Add all hw

---
+++ HW2/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Задача 2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
+System.Console.Write("Введите первое число > ");
+string number1 = Console.ReadLine();
+int num1 = Convert.ToInt32(number1);
+System.Console.Write("Введите второе число > ");
+string number2 = Console.ReadLine();
+int num2 = Convert.ToInt32(number2);
+System.Console.Write("Введите третье число > ");
+string number3 = Console.ReadLine();
+int num3 = Convert.ToInt32(number3);
+int max = 0;
+if (num1 >= num2 && num1 >= num3)
+    max = num1;
+else if (num2 >= num1 && num2 >= num3)
+    max = num2;
+else if (num3 >= num1 && num3 >= num2)
+    max = num3;
+System.Console.WriteLine($"Макимальное число равно {max}");
---
+++ HW3/Program.cs
@@ -0,0 +1,12 @@
+﻿/*Задача 3: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+4 -> да
+-3 -> нет
+7 -> нет
+*/
+System.Console.Write("Введите число > ");
+string number1 = Console.ReadLine();
+int num1 = Convert.ToInt32(number1);
+if (num1%2 == 0)
+System.Console.WriteLine($"Число {num1} чётное");
+else
+System.Console.WriteLine($"Число {num1} не чётное");
